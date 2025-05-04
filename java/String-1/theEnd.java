ba5432c5717ce7740edabde8272b7520b834ee56
sumayyajabeen23 <sumjab2391@gmail.com>
2025-05-03 21:14:28 -0500

    https://codingbat.com/prob/p162477

---
+++ java/String-1/theEnd.java
@@ -0,0 +1,9 @@
+/*Given a string, return a string length 1 from its front, unless front is false, in which case return a string length 1 from its back. The string will be non-empty.*/
+public String theEnd(String str, boolean front) 
+{
+  if(front == true)
+  {
+    return str.substring(0,1);
+  }
+  return str.substring(str.length()-1);
+}
