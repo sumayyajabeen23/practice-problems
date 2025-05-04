08e565e99f972eddeb51c2bee15b6c68be49149b
sumayyajabeen23 <sumjab2391@gmail.com>
2025-05-03 19:34:21 -0500

    https://codingbat.com/prob/p108853

--- java/String-1/extraEnd.java
+++ java/String-1/extraEnd.java
@@ -1,3 +1,4 @@
+/*Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.*/
 public String extraEnd(String str) 
 {
   String str1 = str.substring(str.length()-2);
